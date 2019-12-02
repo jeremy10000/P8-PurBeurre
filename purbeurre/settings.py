439b7d42fe6b18f2afa00fd93d5979dc98f20be9
jeremy10000 <jeremyjulienvisio@outlook.fr>
2019-12-02 09:12:19 +0100

    settings.py : FR

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'purbeurre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django3',
-        'USER': 'postgres',
-        'PASSWORD': 'bdd',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
     }
 }
 
