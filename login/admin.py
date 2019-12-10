21d0c2328d08545a61143ad0f66c91da53743de7
jeremy10000 <jeremyjulienvisio@outlook.fr>
2019-12-10 12:25:15 +0100

    login app admin

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    """ Admin : User"""
+    list_display = ('email', 'first_name', 'last_name', 'last_login')
+
+
+admin.site.register(User, UserAdmin)
