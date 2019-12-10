b2e99e0b5adc03d089feb3224c9385bff20a0655
jeremy10000 <jeremyjulienvisio@outlook.fr>
2019-12-10 12:24:20 +0100

    Login app forms

---
+++ login/forms.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django import forms
+from django.forms.widgets import PasswordInput, TextInput, EmailInput
+from .models import User
+
+
+class JoinForm(UserCreationForm):
+    """ Registration """
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'first_name',
+            'last_name',
+            'password1',
+            'password2')
+
+    email = forms.EmailField(
+        label="Adresse mail",
+        widget=EmailInput(attrs={'class': 'form-control'}),
+        error_messages={
+            'required': 'Please let us know what to call you!',
+            'unique': 'Please mange let us know what to call you!'})
+
+    password1 = forms.CharField(
+        label="Mot de passe",
+        widget=PasswordInput(attrs={'class': 'form-control'}))
+
+    password2 = forms.CharField(
+        label="Confirmation du mot de passe",
+        widget=PasswordInput(attrs={'class': 'form-control'}))
+
+    first_name = forms.CharField(
+        required=False,
+        label="Prénom (facultatif)",
+        widget=TextInput(attrs={'class': 'form-control'}))
+
+    last_name = forms.CharField(
+        required=False,
+        label="Nom (facultatif)",
+        widget=TextInput(attrs={'class': 'form-control'}))
+
+
+class LoginForm(AuthenticationForm):
+    """ LoginView and CSS """
+
+    # username = email
+    username = forms.EmailField(
+        label="Adresse mail",
+        widget=EmailInput(attrs={'class': 'form-control'}))
+
+    password = forms.CharField(
+        label="Mot de passe",
+        widget=PasswordInput(attrs={'class': 'form-control'}))
