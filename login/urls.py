3a4f7c0f0512b817ffd38f763d68b8373d3c0520
jeremy10000 <jeremyjulienvisio@outlook.fr>
2019-12-10 12:25:48 +0100

    login app urls

--- login/urls.py
+++ login/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .forms import LoginForm
 
 
-""" 
-	for templates
-	login:<name>
+"""
+    for templates
+    login:<name>
 
 """
 app_name = 'login'
 
 urlpatterns = [
-    path('', LoginView.as_view(), name="connect"),
+    path('', LoginView.as_view(authentication_form=LoginForm), name="connect"),
     path('logout/', LogoutView.as_view(), name="disconnect"),
-    #path('join/', views.SignUp.as_view(), name="join"),
-    #path('mypage/', views.mypage, name="mypage"),
+    path('join/', views.Join.as_view(), name="join"),
+    path('mypage/', views.mypage, name="mypage"),
 ]
