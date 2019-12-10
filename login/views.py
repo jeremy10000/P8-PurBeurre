3749e54eef82c81c584f5c6955dbd6f28b9f6183
jeremy10000 <jeremyjulienvisio@outlook.fr>
2019-12-10 12:25:59 +0100

    login app views

--- login/views.py
+++ login/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from django.contrib.auth import login, authenticate, logout
+from .forms import JoinForm
 
-# Create your views here.
+
+class Join(FormView):
+    """Show form and create user if valid."""
+
+    form_class = JoinForm
+    success_url = '/'
+    template_name = 'registration/join.html'
+
+    def form_valid(self, form):
+        """ if form is valid """
+
+        form.save()
+        email = form.cleaned_data.get('email')
+        _password = form.cleaned_data.get('password1')
+        user = authenticate(email=email, password=_password)
+        if user:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+def mypage(request):
+    """ Account profile """
+
+    return render(request, "registration/mypage.html")
